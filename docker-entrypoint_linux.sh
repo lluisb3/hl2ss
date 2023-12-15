822f7802a4bd42f3f32c64b705fb85a43f40f188
lluisb3 <lluisb3@gmail.com>
2023-12-15 09:22:18 +0100

    update docker-entrypoint with variables

--- docker-entrypoint_linux.sh
+++ docker-entrypoint_linux.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # bash docker-entrypoint_linux.sh
 
-IMAGE=lluisb3/hl2ss:v2.0
+IMAGE=lluisb3/hl2ss:v3.0
 IP_HOLOLENS=192.168.1.107
+EXP_NAME=hololens_docker
 
 xhost +
 
 sudo docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v ~/docker_volume/hl2ss/data:/home/app/data $IMAGE \
-python3 viewer/advanced_recorder_linux.py --exp_name hololens_docker --ip_hololens $IP_HOLOLENS
+python3 viewer/advanced_recorder_linux.py --exp_name $EXP_NAME --ip_hololens $IP_HOLOLENS
 
 xhost -
